e9b3fb739b1e2cdb1b4de8239cf728c4a346a9eb
pavel_cpp <pasha.dunin.94@gmail.com>
2019-08-27 16:59:26 +0600

    token based auth added

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', views.obtain_auth_token)
 ]
